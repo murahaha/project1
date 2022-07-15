820897891e9e74e0d3d07967c10c47490b79757d
Denys <d9165864615@gmail.com>
2022-07-14 18:38:58 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+"use sctrict";
+
+// alert('HELLO');
+
+// const result = confirm("are you here?");
+// console.log(result);
+
+// const answer = +prompt("are you 18 years old?", "18");
+// console.log(answer + 5);
+
+// const answer2 = prompt("are you 18 years old?", "18");
+// console.log(answer2 + 5);
+
+// const answers = [];
+// answers[0] = prompt("what is your name?", "");
+// answers[1] = prompt("how old are you?", "");
+// answers[2] = prompt("what is your family name?", "");
+// document.write(answers);
+
+// const category = 'toys';
+// console.log('https://someurl.com/' + category);
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/4`);
+
+const user = "ivan";
+alert(`hi, ${user}`);
